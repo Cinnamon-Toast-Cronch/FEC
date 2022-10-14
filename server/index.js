2f55d328e594266823479cee65cc88cfa9670ae3
KateFeaster <zencornhopper@gmail.com>
2022-10-14 12:23:53 -0700

    Minor syntax changes to comply with linter

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const axios = require('axios');
+
 const app = express();
 
 const ATELIER_API_URL = `https://app-hrsei-api.herokuapp.com/api/fec2/${process.env.CAMPUS_CODE}/`;
@@ -16,8 +17,8 @@ app.all('/*', (req, res) => {
     method: req.method,
     baseURL: ATELIER_API_URL,
     headers: { Authorization: process.env.ATELIER_API_TOKEN },
-    data: req.body
-  }
+    data: req.body,
+  };
 
   axios(axiosConfig)
     .then((response) => {
@@ -31,7 +32,7 @@ app.all('/*', (req, res) => {
         res.sendStatus(400);
       }
     });
-})
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`FEC listening on port ${process.env.PORT}`);
