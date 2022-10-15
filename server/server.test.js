558d3bd58e559bacb75b9077fd23291c3ad5793b
KateFeaster <zencornhopper@gmail.com>
2022-10-14 18:08:19 -0700

    Implemented tests to ensure that the express app is returning the expected values

---
+++ server/server.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+const app = require('./index');
+
+describe('index.js module', () => {
+  describe('Static file serving at root', () => {
+    test('It should response the GET method', (done) => {
+      request(app)
+        .get('/')
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          const contentType = response.header['content-type'];
+          expect(contentType.includes('text/html')).toBe(true);
+          done();
+        });
+    });
+
+    test('It should reject the POST method', (done) => {
+      request(app)
+        .post('/')
+        .then((response) => {
+          expect(response.statusCode).toBe(405);
+          done();
+        });
+    });
+  });
+  describe("app.all('/*') route", () => {
+    test('It should response the GET method to /products', (done) => {
+      request(app)
+        .get('/products')
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(Array.isArray(response.body)).toBe(true);
+          const contentType = response.header['content-type'];
+          expect(contentType.includes('application/json')).toBe(true);
+          done();
+        });
+    });
+
+    test('It should reject to an invalid route', (done) => {
+      request(app)
+        .get('/iawfopiajwfoajwf')
+        .then((response) => {
+          expect(response.statusCode).toBe(404);
+          done();
+        });
+    });
+  });
+});
