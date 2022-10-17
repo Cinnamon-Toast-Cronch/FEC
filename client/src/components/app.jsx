d01a1a73af93753bbf974e705c459b329557e91a
Taylor <taylor.sheets3@gmail.com>
2022-10-17 13:42:34 -0700

    implement app skeleton

---
+++ client/src/components/app.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+import ProductDetails from './productDetails'; // TODO - fill in filepath
+import Qna from './questionsAnswers'; // TODO - fill in filepath
+import Rnr from './ratingsReviews'; // TODO - fill in filepath
+import RelatedItems from './relatedItems'; // TODO - fill in filepath
+
+const { useState, useEffect } = React;
+
+function App() {
+  // product points to a product_id?
+  const [product, setProduct] = useState();
+  // setProduct on page load
+  useEffect(() => {
+    axios.get('/products')
+      .then((data) => {
+        setProduct(data[0].id);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="banner">
+        <h1 className="appTitle">App Title: Hello World</h1>
+      </div>
+      <div className="widgets">
+        <ProductDetails product={product} />
+        <Qna product={product} />
+        <Rnr product={product} />
+        <RelatedItems product={product} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
