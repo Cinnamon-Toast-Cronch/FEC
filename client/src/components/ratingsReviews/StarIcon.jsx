fd344807d25bcee2b41236f84bc20df46d16bb54
KateFeaster <zencornhopper@gmail.com>
2022-10-17 16:34:48 -0700

    Rewrote StarIcon to display by overlapping a filled star with an empty star to be able to handle quarter full and three quarters full stars

--- client/src/components/ratingsReviews/StarIcon.jsx
+++ client/src/components/ratingsReviews/StarIcon.jsx
@@ -1,30 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FullStar from '../../assets/images/starIcons/star.svg';
-import HalfStar from '../../assets/images/starIcons/star-half.svg';
 import EmptyStar from '../../assets/images/starIcons/star-outline.svg';
 
+const ICON_SIZE = 20;
+const FILL_STYLE_SETTINGS = {
+  empty: {
+    altText: 'Rating star that is empty',
+    fillAmount: ICON_SIZE * 0,
+  },
+  quarter: {
+    altText: 'Rating star that is twenty-five percent full',
+    fillAmount: ICON_SIZE * 0.4,
+  },
+  half: {
+    altText: 'Rating star that is half way full',
+    fillAmount: ICON_SIZE * 0.5,
+  },
+  threeQuarters: {
+    altText: 'Rating star that is seventy-five percent full',
+    fillAmount: ICON_SIZE * 0.6,
+  },
+  full: {
+    altText: 'Rating star that is completely full',
+    fillAmount: ICON_SIZE * 1,
+  },
+};
+
 function StarIcon({ fillStyle, id }) {
-  const iconAltTexts = {
-    full: 'Rating star icon that is completely filled',
-    half: 'Rating star icon that is half filled',
-    empty: 'Rating star icon that is empty',
-  };
-
-  const iconImages = {
-    full: FullStar,
-    half: HalfStar,
-    empty: EmptyStar,
-  };
-
   return (
-    <img
-      src={iconImages[fillStyle]}
+    <div
+      className="star-outer"
+      style={{
+        backgroundImage: `url(${EmptyStar})`,
+        display: 'inline-block',
+        position: 'relative',
+        width: `${ICON_SIZE}px`,
+        height: `${ICON_SIZE}px`,
+      }}
       key={id}
-      width="20px"
-      height="20px"
-      alt={iconAltTexts[fillStyle]}
-    />
+    >
+      <img
+        className="star-inner"
+        src={FullStar}
+        style={{
+          position: 'absolute',
+          clip: `rect(0, ${FILL_STYLE_SETTINGS[fillStyle].fillAmount}px, ${ICON_SIZE}px, 0)`,
+          width: `${ICON_SIZE}px`,
+          height: `${ICON_SIZE}px`,
+        }}
+        alt={FILL_STYLE_SETTINGS[fillStyle].altText}
+      />
+    </div>
   );
 }
 
