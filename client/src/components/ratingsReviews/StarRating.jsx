e3432d490958541dca31ddc41a29d1c7b3db5c08
KateFeaster <zencornhopper@gmail.com>
2022-10-17 16:36:06 -0700

    Refactored starsArray gnerator loop to appear more clean

--- client/src/components/ratingsReviews/StarRating.jsx
+++ client/src/components/ratingsReviews/StarRating.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import StarIcon from './StarIcon';
+import StarIcon from './StarIcon.jsx';
 
 function StarRating({ rating }) {
   const starsArray = [];
   for (let i = 0; i < 5; i += 1) {
+    let fillStyle;
     if (rating >= i + 1) {
-      starsArray[i] = <StarIcon fillStyle="full" id={i} />;
+      fillStyle = 'full';
+    } else if (rating >= i + 0.75) {
+      fillStyle = 'threeQuarters';
     } else if (rating >= i + 0.5) {
-      starsArray[i] = <StarIcon fillStyle="half" id={i} />;
+      fillStyle = 'half';
+    } else if (rating >= i + 0.25) {
+      fillStyle = 'quarter';
     } else {
-      starsArray[i] = <StarIcon fillStyle="empty" id={i} />;
+      fillStyle = 'empty';
     }
+
+    starsArray[i] = <StarIcon fillStyle={fillStyle} id={i} />;
   }
 
   return <div>{starsArray}</div>;
