905d02a9b6aa0bd4cd3881db063360082630fccd
KateFeaster <zencornhopper@gmail.com>
2022-10-18 12:30:27 -0700

    Created distribution bar component to display the ratio between reviews with a certain rating and the total number of reviews

---
+++ client/src/components/ratingsReviews/RatingDistributionBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function RatingDistributionBar({ reviewCount, totalReviewCount }) {
+  return (
+    <div
+      className="distribution-background"
+      style={{ height: '10px', backgroundColor: '#707070' }}
+    >
+      <div
+        className="distribution-foreground"
+        style={{
+          width: `${(reviewCount / totalReviewCount) * 100}%`,
+          height: '100%',
+          backgroundColor: '#73B98C',
+        }}
+      />
+    </div>
+  );
+}
+
+RatingDistributionBar.propTypes = {
+  reviewCount: PropTypes.number.isRequired,
+  totalReviewCount: PropTypes.number.isRequired,
+};
+
+export default RatingDistributionBar;
