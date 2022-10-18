774c069cff079b762e20eb0c6746fc9376286b33
KateFeaster <zencornhopper@gmail.com>
2022-10-17 18:50:51 -0700

    Created Rating Summary component to display the widget title, average rating, and total number of reviews

---
+++ client/src/components/ratingsReviews/RatingSummary.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import _ from 'underscore';
+import StarRating from './StarRating.jsx';
+
+function RatingSummary({ metaData }) {
+  const ratingCount = _.reduce(
+    metaData.ratings,
+    (memo, num) => memo + parseInt(num, 10),
+    0
+  );
+  const avgRating =
+    _.reduce(
+      metaData.ratings,
+      (memo, num, key) => memo + num * parseInt(key, 10),
+      0
+    ) / ratingCount;
+
+  const roundedRating = Math.round(avgRating * 10) / 10;
+
+  return (
+    <>
+      <h4>RATINGS &amp; REVIEWS</h4>
+      <p>{roundedRating}</p>
+      <StarRating rating={roundedRating} />
+      <p>average of {ratingCount} reviews</p>
+    </>
+  );
+}
+
+export default RatingSummary;
