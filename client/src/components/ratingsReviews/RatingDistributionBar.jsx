f3195951870604ad54631e727daa9757868a87c8
KateFeaster <zencornhopper@gmail.com>
2022-10-18 15:15:57 -0700

    Moved static inline styles to stylesheet

--- client/src/components/ratingsReviews/RatingDistributionBar.jsx
+++ client/src/components/ratingsReviews/RatingDistributionBar.jsx
@@ -3,16 +3,11 @@ import PropTypes from 'prop-types';
 
 function RatingDistributionBar({ reviewCount, totalReviewCount }) {
   return (
-    <div
-      className="distribution-background"
-      style={{ height: '100%', backgroundColor: '#707070' }}
-    >
+    <div className="distribution-background">
       <div
         className="distribution-foreground"
         style={{
           width: `${(reviewCount / totalReviewCount) * 100}%`,
-          height: '100%',
-          backgroundColor: '#73B98C',
         }}
       />
     </div>
