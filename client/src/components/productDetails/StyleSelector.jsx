59bda99a4b94ea9b7eaca07d1704caeb08196f1f
Kally Cao <kallycao98@gmail.com>
2022-10-18 03:57:10 -0700

    Implemented click functionality to show the correct style name

--- client/src/components/productDetails/StyleSelector.jsx
+++ client/src/components/productDetails/StyleSelector.jsx
@@ -2,27 +2,30 @@ import React from 'react';
 
 const { useState, useEffect } = React;
 
-function StyleSelector({ styles }) {
-  const [selectedStyle, setSelectedStyle] = useState({});
+function StyleSelector({ styles, setSelectedStyle, selectedStyle }) {
+  // const [selectedStyle, setSelectedStyle] = useState({});
 
-  useEffect(() => {
-    setSelectedStyle(styles[0]);
-  }, [styles]);
+  // useEffect(() => {
+  //   setSelectedStyle(styles[0]);
+  // }, [styles]);
 
-  const handleStyle = (e) => {
-    console.log(e.target.value)
-    //onClick of thumbnail --> rerender
+  const handleStyle = (style) => {
+    setSelectedStyle(style);
   };
 
+ console.log('selectedStyle', selectedStyle)
   return (
     <div className="stylesContainer">
       <div className="selectedStyle">
         <p>Style > {selectedStyle ? selectedStyle.name : null}</p>
         <div className="styleThumbnails">
-          {styles.map((style) => <input type="image" key={style.style_id} className="button" value={style} onClick={handleStyle} alt={style.name} src={style.photos[0].thumbnail_url}></input>)}
+          {styles.map((style) => <input type="image" key={style.style_id} onClick={(e) => handleStyle(style)} alt={style.name} src={style.photos[0].thumbnail_url}></input>)}
         </div>
       </div>
     </div>
+    // <AddToCartButton/>
+    // <QuantitySelector/>
+    // <SizeSelector/>
 
 
   );
