cedc6b3f4f90a9c829c55c7534484e726388eda6
KateFeaster <zencornhopper@gmail.com>
2022-10-18 14:39:57 -0700

    Created static layout for RatingBreakdownRow that displays the rating, a distribution bar for the rating, and the toal number of ratings

--- client/src/components/ratingsReviews/RatingBreakdownRow.jsx
+++ client/src/components/ratingsReviews/RatingBreakdownRow.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import RatingDistributionBar from './RatingDistributionBar';
 
-function RatingBreakdownRow() {}
+function RatingBreakdownRow() {
+  return (
+    <div className="rating-breakdown-row">
+      <text>5 stars</text>
+      <RatingDistributionBar reviewCount={5} totalReviewCount={10} />
+      <text>13</text>
+    </div>
+  );
+}
 
 export default RatingBreakdownRow;
