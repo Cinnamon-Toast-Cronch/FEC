d76894bdc0413eba25829153b2e2a099808a4790
KateFeaster <zencornhopper@gmail.com>
2022-10-18 15:21:09 -0700

    Added component skeleton to statically render 5 distribution bar rows

--- client/src/components/ratingsReviews/RatingBreakdown.jsx
+++ client/src/components/ratingsReviews/RatingBreakdown.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'underscore';
+import RatingBreakdownRow from './RatingBreakdownRow.jsx';
 
-function RatingBreakdown({ metadata }) {}
+function RatingBreakdown({ metadata }) {
+  return (
+    <div className="rating-breakdown">
+      {_.map(metadata.ratings, (count, val) => (
+        <RatingBreakdownRow key={val} />
+      ))}
+    </div>
+  );
+}
+
+RatingBreakdown.propTypes = {
+  metadata: PropTypes.object.isRequired,
+};
 
 export default RatingBreakdown;
