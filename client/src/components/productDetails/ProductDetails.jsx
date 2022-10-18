2382ae2c4a76214266e8dc129b21b54a0856c7c9
Kally Cao <kallycao98@gmail.com>
2022-10-17 17:44:50 -0700

    Save current progress with ProductDetails component to pull new changes into main branch

--- client/src/components/productDetails/ProductDetails.jsx
+++ client/src/components/productDetails/ProductDetails.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import axios from 'axios';
 
+const { useState } = React;
+
 function ProductDetails({ product }) {
+  const [reviews, setReviews] = useState([]);
+  if (product.id !== undefined) {
+    axios.get(`/reviews/?product_id=${product.id}`)
+      .then(({ data }) => {
+        setReviews(data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  // let ratingTotal = 0;
+  // for (let i = 0; i < reviews.length; i++) {
+  //   ratingTotal += reviews[i].rating;
+  // }
+  // const avgRating = ratingTotal / reviews.length;
+
   return (
     <p>
       Title:
@@ -23,6 +41,16 @@ function ProductDetails({ product }) {
       {product.slogan}
       <br />
       {product.description}
+      <br />
+      Read all
+      {' '}
+      {reviews.length}
+      {' '}
+      reviews.
+      {/* <br />
+      Average Rating:
+      {' '}
+      {avgRating} */}
     </p>
 
   );
