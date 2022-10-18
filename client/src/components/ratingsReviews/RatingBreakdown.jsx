c5dad6ccc70bb45a4dd570282ae475fe370ac1e3
KateFeaster <zencornhopper@gmail.com>
2022-10-18 15:29:20 -0700

    Changed component to dynamically render breakdowns based on data contained in the metadata object

--- client/src/components/ratingsReviews/RatingBreakdown.jsx
+++ client/src/components/ratingsReviews/RatingBreakdown.jsx
@@ -4,10 +4,21 @@ import _ from 'underscore';
 import RatingBreakdownRow from './RatingBreakdownRow.jsx';
 
 function RatingBreakdown({ metadata }) {
+  const totalRatingCount = _.reduce(
+    metadata.ratings,
+    (memo, num) => memo + parseInt(num, 10),
+    0
+  );
+
   return (
     <div className="rating-breakdown">
-      {_.map(metadata.ratings, (count, val) => (
-        <RatingBreakdownRow key={val} />
+      {_.map(metadata.ratings, (count, rating) => (
+        <RatingBreakdownRow
+          key={rating}
+          rating={rating}
+          ratingCount={count}
+          totalRatingCount={totalRatingCount}
+        />
       ))}
     </div>
   );
