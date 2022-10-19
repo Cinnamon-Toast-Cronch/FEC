cc095f221fc3a588fdd934bc0c0897f86895edce
KateFeaster <zencornhopper@gmail.com>
2022-10-19 16:42:05 -0700

    Created Rating Review container to house the rest of the rating and review componenets

---
+++ client/src/components/ratingsReviews/RatingReviewContainer.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import _ from 'underscore';
+import Axios from 'axios';
+import RatingSummary from './RatingSummary.jsx';
+import RatingBreakdown from './RatingBreakdown.jsx';
+
+function RatingReviewContainer({ product }) {
+  const [metadata, setMetadata] = useState({});
+  const [filters, setFilters] = useState([]);
+
+  const addFilter = (newFilter) => {
+    setFilters([...filters, newFilter].sort());
+  };
+
+  const resetFilters = () => {
+    setFilters([]);
+  };
+
+  useEffect(() => {
+    if (!_.isEmpty(product)) {
+      Axios.get(`/reviews/meta?product_id=${product.id}`).then((result) => {
+        setMetadata(result.data);
+      });
+    }
+  }, [product]);
+
+  return (
+    <div className="rating-review-container">
+      <div className="rating-review-left-column">
+        <RatingSummary ratings={metadata.ratings} />
+        <RatingBreakdown
+          ratings={metadata.ratings}
+          filters={filters}
+          addFilter={addFilter}
+          resetFilters={resetFilters}
+        />
+      </div>
+      <div className="rating-review-right-column">
+        <p>Placeholder right column</p>
+      </div>
+    </div>
+  );
+}
+
+RatingReviewContainer.propTypes = {
+  product: PropTypes.object.isRequired,
+};
+
+export default RatingReviewContainer;
