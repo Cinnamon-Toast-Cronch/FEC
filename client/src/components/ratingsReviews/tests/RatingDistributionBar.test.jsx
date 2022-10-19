0eff774ccf7b19c579b740e89a61fe2ac494303b
KateFeaster <zencornhopper@gmail.com>
2022-10-19 12:20:55 -0700

    Removed unnecessary setting header

--- client/src/components/ratingsReviews/tests/RatingDistributionBar.test.jsx
+++ client/src/components/ratingsReviews/tests/RatingDistributionBar.test.jsx
@@ -1,7 +1,3 @@
-/**
- * @jest-environment jsdom
- */
-
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import RatingDistributionBar from '../RatingDistributionBar.jsx';
