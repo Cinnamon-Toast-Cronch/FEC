325d83e22cc872dc3da0c2ec3481b3037aad5120
KateFeaster <zencornhopper@gmail.com>
2022-10-19 12:19:34 -0700

    Rewrote tests to be meaningful to the component

--- client/src/components/ratingsReviews/tests/RatingDistributionBar.test.jsx
+++ client/src/components/ratingsReviews/tests/RatingDistributionBar.test.jsx
@@ -4,15 +4,58 @@
 
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import RatingSummary from '../RatingSummary.jsx';
-import sampleData from '../sampleData/reviewsMeta.js';
+import RatingDistributionBar from '../RatingDistributionBar.jsx';
 
 afterEach(cleanup);
 
-describe('This will test the component', () => {
-  test('renders message', () => {
-    const { getByText } = render(<RatingSummary metadata={sampleData} />);
+describe('Rating distribution bar', () => {
+  test('rating distribution bar should be a distribution backgrou that contains a distribution foreground', () => {
+    const { container } = render(
+      <RatingDistributionBar ratingCount={5} totalRatingCount={10} />
+    );
 
-    expect(getByText('RATINGS & REVIEWS')).toBeInTheDocument();
+    const backgroundElement = container.firstChild;
+    const foregroundElement = backgroundElement.firstChild;
+
+    expect(backgroundElement.tagName).toBe('DIV');
+    expect(backgroundElement).toHaveClass('distribution-background');
+    expect(foregroundElement.tagName).toBe('DIV');
+    expect(foregroundElement).toHaveClass('distribution-foreground');
+  });
+
+  test('distribution-foreground should have a width of 0% when there are no ratings for that rating ', () => {
+    const { container } = render(
+      <RatingDistributionBar ratingCount={0} totalRatingCount={10} />
+    );
+
+    const foregroundElement = container.firstChild.firstChild;
+
+    expect(foregroundElement).toHaveStyle({
+      width: '0%',
+    });
+  });
+
+  test('distribution-foreground should have a width of 50% when the rating count for that rating is half of the total ratings', () => {
+    const { container } = render(
+      <RatingDistributionBar ratingCount={5} totalRatingCount={10} />
+    );
+
+    const foregroundElement = container.firstChild.firstChild;
+
+    expect(foregroundElement).toHaveStyle({
+      width: '50%',
+    });
+  });
+
+  test('distribution-foreground shoudl have a width of 100% when the rating count for that rating equals the totalRatingCount', () => {
+    const { container } = render(
+      <RatingDistributionBar ratingCount={10} totalRatingCount={10} />
+    );
+
+    const foregroundElement = container.firstChild.firstChild;
+
+    expect(foregroundElement).toHaveStyle({
+      width: '100%',
+    });
   });
 });
