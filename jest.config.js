6ada77f99f8149ba71fe1ed7e47f2b9ec3320f12
KateFeaster <zencornhopper@gmail.com>
2022-10-19 10:54:51 -0700

    Added transformIgnorePatterns config to allow axios to work in testing

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ const config = {
   },
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(axios)/)'],
 };
 
 module.exports = config;
