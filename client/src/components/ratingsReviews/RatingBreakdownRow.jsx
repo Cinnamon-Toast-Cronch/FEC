202a8ae0cf37f34d2314aa39cc07c43c89b5c149
KateFeaster <zencornhopper@gmail.com>
2022-10-18 19:12:37 -0700

    Changed rating label to a button that adds that rating's filter when clicked

--- client/src/components/ratingsReviews/RatingBreakdownRow.jsx
+++ client/src/components/ratingsReviews/RatingBreakdownRow.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import RatingDistributionBar from './RatingDistributionBar.jsx';
 
-function RatingBreakdownRow({ rating, ratingCount, totalRatingCount }) {
+function RatingBreakdownRow({
+  rating,
+  ratingCount,
+  totalRatingCount,
+  addFilter,
+}) {
   return (
     <div className="rating-breakdown-row">
-      <p>{`${rating} stars`}</p>
+      <button
+        type="button"
+        className="text-like-button"
+        onClick={() => addFilter(rating)}
+      >
+        {`${rating} stars`}
+      </button>
       <RatingDistributionBar
         ratingCount={ratingCount}
         totalRatingCount={totalRatingCount}
@@ -19,6 +30,7 @@ RatingBreakdownRow.propTypes = {
   rating: PropTypes.string.isRequired,
   ratingCount: PropTypes.number.isRequired,
   totalRatingCount: PropTypes.number.isRequired,
+  addFilter: PropTypes.func.isRequired,
 };
 
 export default RatingBreakdownRow;
