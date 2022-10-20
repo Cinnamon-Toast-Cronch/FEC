b9299caf9bc06a18d8a5600ae128f9a5078e729c
Kally Cao <kallycao98@gmail.com>
2022-10-20 16:00:36 -0700

    implement skeleton for user interactions on the main image

--- client/src/components/productDetails/MainImage.jsx
+++ client/src/components/productDetails/MainImage.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 function MainImage({ imageUrl }) {
+  //   -HOVER: mouse icon changes to magnifying glass
+  // -CLICK on image -->  gallery changes to expanded view
+  const mouseToMagGlass = () => {
+    console.log('Hovering');
+  };
+
+  const expandView = () => {
+    console.log('Expand View');
+  };
   return (
     <div>
-      <img src={imageUrl} alt="placeholder" />
+      <img src={imageUrl} alt="placeholder" onMouseOver={mouseToMagGlass} onClick={expandView} />
     </div>
   );
 }
