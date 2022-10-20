7f6ec9ae25ef7af56e7e3eba6167439faba4a214
KateFeaster <zencornhopper@gmail.com>
2022-10-20 13:37:59 -0700

    Created new states for storing review data and created helper functions to help set them accordingly

--- client/src/components/ratingsReviews/RatingReviewContainer.jsx
+++ client/src/components/ratingsReviews/RatingReviewContainer.jsx
@@ -4,10 +4,14 @@ import _ from 'underscore';
 import Axios from 'axios';
 import RatingSummary from './RatingSummary.jsx';
 import RatingBreakdown from './RatingBreakdown.jsx';
+import ReviewTile from './ReviewTile.jsx';
 
 function RatingReviewContainer({ product }) {
   const [metadata, setMetadata] = useState({});
   const [filters, setFilters] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [page, setPage] = useState(0);
+  const [sort, setSort] = useState('helpful');
 
   const addFilter = (newFilter) => {
     if (!filters.includes(newFilter)) {
@@ -19,11 +23,30 @@ function RatingReviewContainer({ product }) {
     setFilters([]);
   };
 
+  const resetAndGetReviews = () => {
+    Axios.get(`/reviews?product_id=${product.id}&sort=${sort}`).then(
+      (response) => {
+        setPage(1);
+        setReviews(response.data.results);
+      }
+    );
+  };
+
+  const loadMoreReviews = () => {
+    Axios.get(
+      `/reviews?product_id=${product.id}&page=${page + 1}&sort=${sort}`
+    ).then((response) => {
+      setPage(page + 1);
+      setReviews([...reviews, response.data.results]);
+    });
+  };
+
   useEffect(() => {
     if (!_.isEmpty(product)) {
       Axios.get(`/reviews/meta?product_id=${product.id}`).then((result) => {
         setMetadata(result.data);
       });
+      resetAndGetReviews();
     }
   }, [product]);
 
@@ -39,7 +62,9 @@ function RatingReviewContainer({ product }) {
         />
       </div>
       <div className="rating-review-right-column">
-        <p>Placeholder right column</p>
+        {reviews.map((review) => (
+          <ReviewTile review={review} key={review.review_id} />
+        ))}
       </div>
     </div>
   );
