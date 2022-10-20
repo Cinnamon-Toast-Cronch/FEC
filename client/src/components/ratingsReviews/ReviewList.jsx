7d2832edd756b0af30cffa98d8bb7be39c050a27
KateFeaster <zencornhopper@gmail.com>
2022-10-20 14:53:43 -0700

    Added sort dropdown for user to choose sort method

--- client/src/components/ratingsReviews/ReviewList.jsx
+++ client/src/components/ratingsReviews/ReviewList.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Axios from 'axios';
-import ReviewTile from './reviewTile.jsx';
+import ReviewTile from './eviewTile.jsx';
 
 function ReviewList({ productId }) {
   const [reviews, setReviews] = useState([]);
   const [page, setPage] = useState(0);
-  const [sort, setSort] = useState('helpful');
+  const [sort, setSort] = useState('relevant');
 
   const loadMoreReviews = () => {
     Axios.get(
@@ -30,6 +30,18 @@ function ReviewList({ productId }) {
 
   return (
     <>
+      <div className="review-sort-bar">
+        <p>Sorted on:</p>
+        <select
+          onChange={(e) => {
+            setSort(e.target.value);
+          }}
+        >
+          <option value="relevant">Relevant</option>
+          <option value="newest">Newest</option>
+          <option value="helpful">Helpful</option>
+        </select>
+      </div>
       {reviews.map((review) => (
         <ReviewTile review={review} key={review.review_id} />
       ))}
