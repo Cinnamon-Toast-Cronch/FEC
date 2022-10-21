953dd050e85fe5320782903472ef2dc806335d9a
Kevin Zhang <kevz.workspace@gmail.com>
2022-10-21 13:59:07 -0700

    Testing github duplicate folders

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-<<<<<<< HEAD
-// import ProductDetails from './productDetails/ProductDetails.jsx';
-// import Qna from './questionsAnswers/Qna.jsx';
-// import Rnr from './ratingsReviews'; // TODO - fill in filepath
-=======
 import ProductDetails from './productDetails/ProductDetails.jsx';
 import Qna from './questionsAnswers/A-QnaWidget.jsx';
 import Rnr from './ratingsReviews/RatingReviewContainer.jsx'; // TODO - fill in filepath
->>>>>>> main
 import RelatedItems from './relatedItems/container.jsx'; // TODO - fill in filepath
 
 const { useState, useEffect } = React;
@@ -29,14 +23,8 @@ function App() {
         <h1 className="appTitle">App Title: Hello World</h1>
       </div>
       <div className="widgets">
-<<<<<<< HEAD
-        {/* <ProductDetails product={product} /> */}
-        {/* <Qna product={product} /> */}
-        {/* <Rnr product={product} /> */}
-=======
         <ProductDetails product={product} />
         <Qna product={product} />
->>>>>>> main
         <RelatedItems product={product} />
         <Rnr product={product} />
       </div>
