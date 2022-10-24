1d4a685174db46b14d901a1a83783e2e9fedc548
KateFeaster <zencornhopper@gmail.com>
2022-10-24 16:53:27 -0700

    Changed the entire row to act as a button. Changed the background color on hover to indicate it is actionable

--- client/src/components/ratingsReviews/RatingBreakdownRow.jsx
+++ client/src/components/ratingsReviews/RatingBreakdownRow.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import RatingDistributionBar from './RatingDistributionBar.jsx';
 
+// TODO: The filters will be toggled on and off with each click.
+// Clicking a second time on a rating breakdown will remove the filter
+// for that rating type. If this action removes the last or only filter,
+// then the list should return to its default state and all reviews should be shown.
+
 function RatingBreakdownRow({
   rating,
   ratingCount,
@@ -9,20 +14,20 @@ function RatingBreakdownRow({
   addFilter,
 }) {
   return (
-    <div className="rating-breakdown-row">
-      <button
-        type="button"
-        className="text-like-button"
-        onClick={() => addFilter(rating)}
-      >
-        {`${rating} stars`}
-      </button>
+    <button
+      className="rating-breakdown-row text-like-button"
+      type="button"
+      onClick={() => {
+        addFilter(rating);
+      }}
+    >
+      <p>{`${rating} stars`}</p>
       <RatingDistributionBar
         ratingCount={ratingCount}
         totalRatingCount={totalRatingCount}
       />
       <p>{ratingCount}</p>
-    </div>
+    </button>
   );
 }
 
