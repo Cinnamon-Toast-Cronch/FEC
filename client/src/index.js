f3064f46e65253b82f3d08a0df00fd5cd0c3bc7e
Taylor <taylor.sheets3@gmail.com>
2022-10-27 12:30:16 -0700

    Synchronizing styles across the app

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,6 @@ import App from './components/app.jsx';
 import './assets/stylesheets/styles.css';
 import './assets/stylesheets/relatedStyles.css';
 import './assets/stylesheets/stylesPD.css';
+import './assets/stylesheets/QnAStyles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
